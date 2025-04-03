f5070eebbe948065df59dc1dead5ea9aa21e79a8
mnick02 <nokiamn3@gmail.com>
2025-04-03 12:34:23 -0700

    Add eventListener for addBtn

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ const btn = document.getElementById("project");
 
 btn.addEventListener("click", () => {
     projectDialog.showModal();
-})
+});
+
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("title").value;
+    console.log(title);
+});
